905b86524e21f80477f6cd7613aabae1cc9b5214
Bkoshik <bakubakosh@gmail.com>
2025-02-18 13:28:15 +0500

    Добавил функциональность секундомера и таймера в помощник

--- assistant.py
+++ assistant.py
@@ -1,7 +1,4 @@
-import requests
-import time
-import webbrowser
-import os
+import requests, time, webbrowser, os, keyboard
 import math # Для 4 варианта
 from datetime import datetime
 date = datetime.now()
@@ -12,7 +9,7 @@ print("\033[38;2;0;180;0m")
 def symbolPrint(str):
     for i in str:
         print(i, end="", flush=True)
-        time.sleep(0.075)
+        # time.sleep(0.1)
     return ""
 
 sys_ques = """SYS> Введите:
@@ -21,6 +18,7 @@ sys_ques = """SYS> Введите:
 2. Узнать погоду
 3. Открыть веб-сайт
 4. Ввести python-команды
+5. Секундомер/Таймер
 
 """
 sys = "SYS> Что вы хотите сделать?\n\n"
@@ -41,8 +39,8 @@ while True:
             symbolPrint(
 f"""
 SYS> Текущее время:
-{date.hour}:{date.minute}
-{date.day}.{date.month}.{date.year}
+{date.hour:02}:{date.minute:02}
+{date.day:02}.{date.month:02}.{date.year}
 
 """
 )
@@ -149,8 +147,72 @@ SYS> Аргументы:
 
 """
 )
+        case "5":
+            timetype = input(symbolPrint("\nSYS> Что вы хотите установить?\n0. Выйти\n1. Секндомер\n2. Таймер\n\n> "))
+
+            match timetype:
+                case "0":
+                    break
+                case "1":
+                    seconds,minutes,hours = 0,0,0
+                    symbolPrint("\nSYS> Секндомер запущен! Нажмите 'q' для выхода.\n\n")
+                    
+                    while True: 
+                        time.sleep(1)
+
+                        if keyboard.is_pressed('q'):  # Выход при нажатии 'q'
+                            symbolPrint("\n\nSYS> Секундомер остановлен.\n\n")
+                            break
+                        
+                        if seconds == 60:
+                            seconds = 0
+                            minutes+=1
+                        if minutes == 60:
+                            minutes = 0
+                            hours+=1
+
+                        if hours > 0:
+                            print(f"Время: {hours:02}:{minutes:02}:{seconds:02}",sep='',end='\r')   
+                        elif minutes > 0:
+                            print(f"Время: {minutes:02}:{seconds:02}",sep='',end='\r')
+                        else: 
+                            print(f"Время: {seconds:02}",sep='',end='\r')
+
+                        seconds+=1
+                case "2":
+                    timertext = input(symbolPrint("\nSYS> О чём вам напомнить?\n\n> "))
+                    timerhour, timermin, timersek = map(int, input(symbolPrint("\nSYS> На сколько времени (h:m:s)?\n\n> ")).split(":"))
+
+                    if timersek > 60:
+                        timersek -= 60
+                        timermin += 1
+                    if timermin > 60:
+                        timermin -= 60
+                        timerhour += 1
+
+                    print()
+                    while timerhour >= 0 and timermin >= 0 and timersek >= 0:
+                        time.sleep(0.1)
+
+                        if keyboard.is_pressed('q'):  # Выход при нажатии 'q'
+                            symbolPrint("\n\nSYS> Секндомер остановлен.\n\n")
+                            break
+                        
+                        if timerhour > 0:
+                            if timermin == 0:
+                                timerhour -= 1
+                                timermin = 60
+                        if timermin > 0:
+                            if timersek == 0:
+                                timermin -= 1
+                                timersek = 60
+
+                        print(f"Время: {timerhour:02}:{timermin:02}:{timersek:02}",sep='',end='\r')  
+                        timersek -= 1
+                    symbolPrint(f"\nНапоминание: {timertext}\n\n")
+                        
         case "clear":
             clear = lambda: os.system('cls')
             clear()
         case _:
-            symbolPrint(sys_err + sys)
+            symbolPrint(sys_err + "\n\n")
