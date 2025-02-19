954196bbdb90e54f1a5b8e1d41e3a7f48392806a
Bkoshik <bakubakosh@gmail.com>
2025-02-19 12:28:54 +0500

    Добавил создание таблицы для хранения данных о погоде и обновил логику вставки данных в базу

--- create and add to db.py
+++ create and add to db.py
@@ -4,7 +4,15 @@ conn = sqlite3.connect("weather.db")
 cursor = conn.cursor()
 id, name, api = map(str, input().split())
 
-cursor.execute("INSERT INTO api (id, name, api) VALUES (?, ?, ?)", (id, name, api))
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS weather (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    api TEXT NOT NULL
+)
+""")
+
+cursor.execute("INSERT INTO weather (id, name, api) VALUES (?, ?, ?)", (id, name, api))
 
 conn.commit()
 conn.close()
