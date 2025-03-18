f7228b2312b227974ce1fe3d52feb4ea61cecec4
bkoshik <bakubakosh@gmail.com>
2025-03-18 13:18:08 +0500

    Просто устал

--- Linux-Assistant/module/calc.py
+++ Linux-Assistant/module/calc.py
@@ -32,7 +32,7 @@ def calc(apicur):
             symbolPrint("\nSYS> " + str(convert_currency(amount, from_currency, to_currency)) + "\n\n")
         case "2":
             date = datetime.now()
-            hfile = f"{}/module/apis.db".format(param('path'))
+            hfile = str(param('path')) + "/module/apis.db"
 
             if not os.path.exists(hfile):
                 open(hfile, "w").close()
